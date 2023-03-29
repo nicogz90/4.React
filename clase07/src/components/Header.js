14415a22bfb28ee1d73604c58b0a5aa205832ff1
nicolasgonzalez <104945758+nicogz90@users.noreply.github.com>
2023-03-29 18:56:32 -0300

    feat: delete and patch ToDo to DB

--- clase07/src/components/Header.js
+++ clase07/src/components/Header.js
@@ -9,12 +9,16 @@ const Header = () => {
   const dispatch = useDispatch();
 
   const onSave = (text) => {
+    // We are using the demo json server, allowing us to create a demo REST API.
+    // JSON Server is a notable Node module ('npm install json-server') that you can use to build demo REST JSON services effortlessly and quickly.
+    // To create the fake REST API, you will only need a JSON file (in this case 'db.json', port 3004 --> see script 'start-api' at package.json) and add some sample data into it.
+    const DB_URL = 'http://localhost:3004/todos';
     dispatch(todoLoading(true));
     // setIsLoading(true);
     axios
-      .post('http://localhost:3004/todos', { text, completed: false })
+      .post(DB_URL, { text, completed: false }) // added to DB (normally this is done on server-side)
       .then((res) => {
-        dispatch(addTodo(text));
+        dispatch(addTodo(text)); // added to state
         dispatch(todoLoading(false));
         // setIsLoading(false);
       });
--- clase07/src/components/TodoItem.js
+++ clase07/src/components/TodoItem.js
@@ -1,9 +1,12 @@
+import axios from 'axios';
 import React from 'react';
+// import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { checkTodo, deleteTodo } from '../redux/todo.slice';
+import { checkTodo, deleteTodo, todoLoading } from '../redux/todo.slice';
 
 export default function TodoItem({ todo }) {
   const dispatch = useDispatch();
+  // const [isLoading, setIsLoading] = useState(false)
   return (
     <li className={todo.completed ? 'completed' : ''}>
       <div className="view">
@@ -11,15 +14,35 @@ export default function TodoItem({ todo }) {
           className="toggle"
           type="checkbox"
           checked={todo.completed}
-          onChange={(e) =>
-            dispatch(checkTodo({ id: todo.id, completed: e.target.checked }))
-          }
+          onChange={(e) => {
+            dispatch(todoLoading(true));
+            // setIsLoading(true);
+            axios
+              .patch(`http://localhost:3004/todos/${todo.id}`, {
+                completed: e.target.checked,
+              }) // patched to DB (normally this is done on server-side)
+              .then((res) => {
+                dispatch(
+                  checkTodo({ id: todo.id, completed: e.target.checked })
+                ); // patched to state
+                dispatch(todoLoading(false));
+                // setIsLoading(false);
+              });
+          }}
         />
         <label>{todo.text}</label>
         <button
           className="destroy"
           onClick={() => {
-            dispatch(deleteTodo(todo.id));
+            dispatch(todoLoading(true));
+            // setIsLoading(true);
+            axios
+              .delete(`http://localhost:3004/todos/${todo.id}`) // deleted from DB (normally this is done on server-side)
+              .then((res) => {
+                dispatch(deleteTodo(todo.id)); // deleted from state
+                dispatch(todoLoading(false));
+                // setIsLoading(false);
+              });
           }}
         />
       </div>
