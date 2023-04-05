9b94b56ba66c597ee6d456c7e2c27f98dc958a92
nicolasgonzalez <104945758+nicogz90@users.noreply.github.com>
2023-04-05 11:19:17 -0300

    chore: added comments

--- clase10/App.js
+++ clase10/App.js
@@ -1,16 +1,23 @@
 import { StatusBar } from "expo-status-bar";
-import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import Main from "./components/Main";
 
 export default function App() {
   return (
+    /* react-native-safe-area-context provides a flexible API for accessing device safe area inset information. 
+    This allows you to position your content appropriately around notches, status bars, home indicators, and other such device and operating system interface elements. 
+    It also provides a SafeAreaView component that you can use in place of View to automatically inset your views to account for safe areas. */
     <SafeAreaView style={styles.container}>
+      {/* HEADER */}
       <View style={styles.header}>
         <Text style={styles.headerText}>
           Welcome to my first React Native app
         </Text>
       </View>
+      {/* MAIN */}
       <Main />
+      {/* FOOTER */}
       <View style={styles.footer}>
         <Text style={{ color: "white" }}>Made with React Native</Text>
       </View>
@@ -22,10 +29,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "light",
+    backgroundColor: "gray",
   },
   header: {
-    flex: 1,
+    height: 80,
     backgroundColor: "gray",
     alignItems: "center",
     justifyContent: "center",
@@ -35,8 +42,8 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   footer: {
-    flex: 1,
-    backgroundColor: "grey",
+    height: 60,
+    backgroundColor: "gray",
     color: "white",
     alignItems: "center",
     justifyContent: "center",
--- clase10/components/Main.jsx
+++ clase10/components/Main.jsx
@@ -1,20 +1,25 @@
-import { useState } from "react";
-import { View, Text, TextInput, Button, Image, StyleSheet } from "react-native";
-import titanic from "../assets/titanic.png";
+import { View, TextInput, Button, Image, StyleSheet } from "react-native";
 
 export default function Main() {
   return (
     <View style={styles.main}>
-      <View style={{ flexDirection: "row", padding: 10 }}>
+      <View style={{ flexDirection: "row", marginBottom: 10 }}>
         <TextInput
-          style={{ borderWidth: 1, width: 200, padding: 10, borderRadius: 15 }}
+          style={{
+            borderWidth: 1,
+            width: 200,
+            paddingLeft: 10,
+            marginRight: 10,
+            borderRadius: 15,
+          }}
           placeholder="Enter movie..."
         />
         <Button title="Search" />
       </View>
-      {/* IMAGEN FIJA (EN LOS SIGUIENTES EJERCICIOS SE HACEN LLAMADAS A UNA API) */}
+      {/* Imagen fija (en los siguientes ejercicios se hacen llamadas a una API) */}
       <Image
         style={{ width: "80%", height: "80%" }}
+        // source={require("../assets/titanic.png")}
         source={{
           uri: "https://m.media-amazon.com/images/M/MV5BMDdmZGU3NDQtY2E5My00ZTliLWIzOTUtMTY4ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg",
         }}
@@ -25,8 +30,9 @@ export default function Main() {
 
 const styles = StyleSheet.create({
   main: {
-    flex: 5,
-    backgroundColor: "light",
+    flex: 1,
+    backgroundColor: "lightgray",
     alignItems: "center",
+    justifyContent: "center",
   },
 });
