1c476eab180cbaa86077e21467f14aea6c492c77
nicolasgonzalez <104945758+nicogz90@users.noreply.github.com>
2023-06-26 22:07:38 -0300

    added initial username if previously logged

--- proyecto-final-react/src/index.js
+++ proyecto-final-react/src/index.js
@@ -5,6 +5,32 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
+// import api from "./api";
+import jwt_decode from "jwt-decode";
+import { setUsername } from "./redux/user.slice";
+
+const token = localStorage.getItem("token");
+
+if (token) {
+  const decodedToken = jwt_decode(token);
+  console.log(decodedToken);
+  store.dispatch(setUsername(decodedToken.username));
+}
+
+/* If username is not in JWT payload: */
+//   fetchUsername(decodedToken?.id)
+//     .then((username) => store.dispatch(setUsername(username)))
+//     .catch((error) => console.log("Error fetching username:", error));
+// }
+
+// function fetchUsername(id) {
+//   return new Promise((resolve, reject) => {
+//     api
+//       .get(`/users/${id}`)
+//       .then((res) => resolve(res.data.username))
+//       .catch((error) => reject(error));
+//   });
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
